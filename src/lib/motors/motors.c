dc1ab29ec16bc78796c50b8f87a0a9c9a08bb5ec
Chris E <cjeusti100@maine.edu>
2015-11-16 23:13:40 -0500

    Change motors port to use PORTC rather than PORTB (conflicts with FT232 programmer) - note: had to update fuses to disable JTAG interface as it conflicted with normal operation of PORTC

--- src/lib/motors/motors.c
+++ src/lib/motors/motors.c
@@ -3,28 +3,36 @@
 #include <stdbool.h>
 
 /* Motor Information */
-#define MOTOR1A_ON ~(1<<PB0)
-#define MOTOR1B_ON (1<<PB1)
-#define MOTOR2A_ON ~(1<<PB2)
-#define MOTOR2B_ON (1<<PB3)
-#define MOTOR3A_ON ~(1<<PB4)
-#define MOTOR3B_ON (1<<PB5)
-#define MOTOR4A_ON ~(1<<PB6)
-#define MOTOR4B_ON (1<<PB7)
+#define MOTOR1A_ON ~(1<<PC0)
+#define MOTOR1B_ON (1<<PC1)
+#define MOTOR2A_ON ~(1<<PC2)
+#define MOTOR2B_ON (1<<PC3)
+#define MOTOR3A_ON ~(1<<PC4)
+#define MOTOR3B_ON (1<<PC5)
+#define MOTOR4A_ON ~(1<<PC6)
+#define MOTOR4B_ON (1<<PC7)
 
-#define MOTORS_PORT PORTB
-#define MOTORS_PORT_DIR DDRB
+/* Define port to use for motors */
+#define MOTORS_PORT PORTC
+#define MOTORS_PORT_DIR DDRC
 
-/* Pouring measurements */
-#define TENTH_OUNCE 70.984
+/* Pouring measurements - used for timing */
+#define TENTH_OUNCE 490
 #define QUARTER_OUNCE (2.5 * TENTH_OUNCE)
 #define HALF_OUNCE (5.0 * TENTH_OUNCE)
 #define OUNCE (10.0 * TENTH_OUNCE)
 
+// 3.3814 ounces / minute
+// 0.05635666666 ounces / second
+
+
 // Pump = 1.67 mL / second
-// 0.1 Oz = ~2.957 mL
-// 1.77 seconds / 0.1 ounce
-// 17.7 seconds / 1 ounce
+// 0.1 Oz = 2.95735 mL
+
+
+
+// 1.774412965 seconds / 0.1 ounce
+// 17.74412965 seconds / 1 ounce
 
 static bool is_motor1_on = false;
 static bool is_motor2_on = false;
