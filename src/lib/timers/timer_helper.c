ac1e3f20fc66ed74539dd45d51a14744e97964fa
Chris E <cjeusti100@maine.edu>
2015-11-17 23:15:53 -0500

    Moved motor control functions in here

--- src/lib/timers/timer_helper.c
+++ src/lib/timers/timer_helper.c
@@ -1,6 +1,9 @@
 #include "timer_helper.h"
 #include <avr/io.h>
 
+volatile float tot_overflow;
+static float pouring_length;
+
 void init_motors_timer(void) {
 	// Normal operation of timer
 	TCCR1A = 0x00;
@@ -12,6 +15,22 @@ void init_motors_timer(void) {
 	TCCR1C = 0x00;
 	// Initialize timer overflow count to 0
 	tot_overflow = 0.0;
+}
+
+void enable_motor_timer(int motor) {
+	if(pouring_length != 0) {
+		motor_on(motor);
+
+		enable_motors_timer_interrupt();
+
+		// Wait to be done pouring liquid
+		while (tot_overflow < pouring_length);
+
+		motor_off(motor);
+		tot_overflow = 0;
+
+		disable_motors_timer_interrupt();
+	}
 }
 
 void enable_motors_timer_interrupt(void) {
--- src/lib/timers/timer_helper.h
+++ src/lib/timers/timer_helper.h
@@ -2,8 +2,7 @@
 #define TIMER_HELPER
 #endif
 
-#include <avr/io.h>
-
 void init_motors_timer(void);
+void enable_motor_timer(int motor);
 void enable_motors_timer_interrupt(void);
 void disable_motors_timer_interrupt(void);
