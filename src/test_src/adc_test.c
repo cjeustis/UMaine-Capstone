c67f3c430e6e08367d35d6d30c0056177306e05e
Chris E <cjeusti100@maine.edu>
2015-11-19 09:29:02 -0500

    Updated temp test prog for Steinhart-hart equation implementation (non-linear curve for temp sensor)

--- src/test_src/adc_test.c
+++ src/test_src/adc_test.c
@@ -11,7 +11,6 @@
 #include "../lib/adc/adc.c"
 #include "../lib/usart/usart.c"
 #include "../lib/serial/serial.c"
-#include "../lib/motors/motors.c"
 
 // Set baud for serial communication
 #define BAUD 9600
@@ -54,11 +53,27 @@ float convert_temp(uint16_t adc) {
 }
 
 ISR(ADC_vect) {
+
+  	// Assuming a 10k Thermistor.  Calculation is actually: Resistance = (1024/ADC)
+	double resistance = ((10240000/ADC) - 10000);
+
+	/******************************************************************/
+	/* Utilizes the Steinhart-Hart Thermistor Equation:				*/
+	/*    Temperature in Kelvin = 1 / {A + B[ln(R)] + C[ln(R)]^3}		*/
+	/*    where A = 0.001127587560, B = 0.0002343528476 and C = 0.00000008698059497	*/
+	/******************************************************************/
+
 	if (temps.channel == 0) {
-		temps.temp0F = convert_temp(ADC+TEMP_OFFSET);
+		temps.temp0F = log(resistance);
+		temps.temp0F = 1 / (0.001127587560 + (0.0002343528476 * temps.temp0F) + (0.00000008698059497 * temps.temp0F * temps.temp0F * temps.temp0F));
+		temps.temp0F = temps.temp0F - 273.15;  // Convert Kelvin to Celsius
+		temps.temp0F = (temps.temp0F * 9.0 / 5.0) + 32.0;
 	}
 	else if (temps.channel == 1) {
-		temps.temp1F = convert_temp(ADC+TEMP_OFFSET);
+		temps.temp1F = log(resistance);
+		temps.temp1F = 1 / (0.001127587560 + (0.0002343528476 * temps.temp1F) + (0.00000008698059497 * temps.temp1F * temps.temp1F * temps.temp1F));
+		temps.temp1F = temps.temp1F - 273.15;  // Convert Kelvin to Celsius
+		temps.temp1F = (temps.temp1F * 9.0 / 5.0) + 32.0;
 
 		temps.tempFinal = (temps.temp0F + temps.temp1F) / 2.0;
 		printf("Temperature: %1.2f\n", temps.tempFinal);
@@ -92,10 +107,6 @@ int main(int argc, const char *argv[])
 	// Initialize ADC
 	init_adc(REFERENCE, PRESCALER);
 
-	init_motors();
-
-	DDRB = 0x01;
-
 	// Enable global interrupts
 	sei();
 
@@ -103,12 +114,12 @@ int main(int argc, const char *argv[])
 	{
 		temperature_value();
 
-		if (temps.temp1F >= TEMP_UPPER) {
-			PORTB = 0x01;
-		}
-		else {
-			PORTB = 0x00;
-		}
+		// if (temps.temp1F >= TEMP_UPPER) {
+		// 	PORTB = 0x01;
+		// }
+		// else {
+		// 	PORTB = 0x00;
+		// }
 		// Wait a second
 		_delay_ms(1000);
 	}
