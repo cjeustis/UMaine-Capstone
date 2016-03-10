b31d225e48acc3d266014e08ec87d75c444ac27b
Chris Eustis <ceusti100@maine.edu>
2016-03-10 18:23:02 -0500

    stillt rying to get ingredient amounts sent to avr

--- src/mr-pour-website/php/sendIngredientAmounts.php
+++ src/mr-pour-website/php/sendIngredientAmounts.php
@@ -1,15 +1,15 @@
 <?php
-        if((isset($_POST['control'])) && (isset($_POST['ing_1']) && (isset($_POST['ing_2']) && (isset($_POST['ing_3']) && (isset($_POST['ing_4']))) {
+        if((isset($_POST['control']))) {
 
         $control = $_POST['control'];
-        $ing_1 = $_POST['ing_1'];
-        $ing_2 = $_POST['ing_2'];
-        $ing_3 = $_POST['ing_3'];
-        $ing_4 = $_POST['ing_4'];
+        $ing1 = $_POST['amount_1'];
+        $ing2 = $_POST['amount_2'];
+        $ing3 = $_POST['amount_3'];
+        $ing4 = $_POST['amount_4'];
         }
     echo system("echo \"$control\" > /dev/ttyAMA0 ");
-    echo system("echo \"$ing_1\" > /dev/ttyAMA0 ");
-    echo system("echo \"$ing_2\" > /dev/ttyAMA0 ");
-    echo system("echo \"$ing_3\" > /dev/ttyAMA0 ");
-    echo system("echo \"$ing_4\" > /dev/ttyAMA0 ");
+    echo system("echo \"$ing1\" > /dev/ttyAMA0 ");
+    echo system("echo \"$ing2\" > /dev/ttyAMA0 ");
+    echo system("echo \"$ing3\" > /dev/ttyAMA0 ");
+    echo system("echo \"$ing4\" > /dev/ttyAMA0 ");
 ?>
