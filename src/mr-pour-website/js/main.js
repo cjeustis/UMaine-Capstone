333fda08124707c75b5f2407d6f0f6b3f3dd43ba
Chris Eustis <ceusti100@maine.edu>
2016-03-14 16:43:50 -0400

    Working on the style

--- src/mr-pour-website/js/main.js
+++ src/mr-pour-website/js/main.js
@@ -149,6 +149,57 @@ mrPour.run(function($rootScope, store, $location) {
 });
 
 
+mrPour.service('modalService', ['$modal',
+    function ($modal) {
+        var modalDefaults = {
+            backdrop: true,
+            keyboard: true,
+            modalFade: true,
+            templateUrl: '../html/modal.html'
+        };
+
+        var modalOptions = {
+            closeButtonText: 'Close',
+            actionButtonText: 'OK',
+            headerText: 'Proceed?',
+            bodyText: 'Perform this action?'
+        };
+
+        this.showModal = function (customModalDefaults, customModalOptions) {
+            if (!customModalDefaults) customModalDefaults = {};
+            customModalDefaults.backdrop = 'static';
+            return this.show(customModalDefaults, customModalOptions);
+        };
+
+        this.show = function (customModalDefaults, customModalOptions) {
+            //Create temp objects to work with since we're in a singleton service
+            var tempModalDefaults = {};
+            var tempModalOptions = {};
+
+            //Map angular-ui modal custom defaults to modal defaults defined in service
+            angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+
+            //Map modal.html $scope custom properties to defaults defined in service
+            angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+            if (!tempModalDefaults.controller) {
+                tempModalDefaults.controller = function ($scope, $modalInstance) {
+                    $scope.modalOptions = tempModalOptions;
+                    $scope.modalOptions.ok = function (result) {
+                        $modalInstance.close(result);
+                    };
+                    $scope.modalOptions.close = function (result) {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }
+            }
+
+            return $modal.open(tempModalDefaults).result;
+        };
+    }
+]);
+
+
 //mrPour.factory('MathService', function() {
 //    var factory = {};
 //
@@ -659,7 +710,24 @@ mrPour.controller('updateController', function ($scope, $rootScope, $http, store
     }
 });
 
-mrPour.controller('tempController', function ($scope, $rootScope, $http, store, $location, $anchorScroll) {
+mrPour.controller('tempController', function ($scope, $rootScope, $http, store, $location, modalService, $anchorScroll) {
+
+    $scope.deleteCustomer = function () {
+
+        var custName = $scope.customer.firstName + ' ' + $scope.customer.lastName;
+
+
+        var modalOptions = {
+            closeButtonText: 'Cancel',
+            actionButtonText: 'Delete Customer',
+            headerText: 'Delete ' + custName + '?',
+            bodyText: 'Are you sure you want to delete this customer?'
+        };
+
+        modalService.showModal({}, modalOptions).then(function (result) {
+            console.log("show modal");
+        });
+    };
 
     $scope.val = {};
     $scope.val['control'] = 't';
